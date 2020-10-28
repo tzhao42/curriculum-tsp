fd81389b28ce3188798c79ca00f70f08731e3cfa
Tong Zhao <tongzhao997@gmail.com>
2020-10-28 13:05:10 -0500

    training on new instance

--- train.sh
+++ train.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-nohup python3 trainer.py --debug --run-name 1.00-0.00-0.00 --proportions 1.00 0.00 0.00 &
+nohup python3 trainer.py --run-name 1.00-0.00-0.00 --proportions 1.00 0.00 0.00 &
 
-nohup python3 trainer.py --debug --run-name 0.85-0.15-0.00 --proportions 0.85 0.15 0.00 &
-nohup python3 trainer.py --debug --run-name 0.50-0.50-0.00 --proportions 0.50 0.50 0.00 &
-nohup python3 trainer.py --debug --run-name 0.15-0.85-0.00 --proportions 0.15 0.85 0.00 &
-nohup python3 trainer.py --debug --run-name 0.00-1.00-0.00 --proportions 0.00 1.00 0.00 &
+nohup python3 trainer.py --run-name 0.85-0.15-0.00 --proportions 0.85 0.15 0.00 &
+nohup python3 trainer.py --run-name 0.50-0.50-0.00 --proportions 0.50 0.50 0.00 &
+nohup python3 trainer.py --run-name 0.15-0.85-0.00 --proportions 0.15 0.85 0.00 &
+nohup python3 trainer.py --run-name 0.00-1.00-0.00 --proportions 0.00 1.00 0.00 &
 
-nohup python3 trainer.py --debug --run-name 0.85-0.00-0.15 --proportions 0.85 0.00 0.15 &
-nohup python3 trainer.py --debug --run-name 0.50-0.00-0.50 --proportions 0.50 0.00 0.50 &
-nohup python3 trainer.py --debug --run-name 0.15-0.00-0.85 --proportions 0.15 0.00 0.85 &
-nohup python3 trainer.py --debug --run-name 0.00-0.00-1.00 --proportions 0.00 0.00 1.00 &
+nohup python3 trainer.py --run-name 0.85-0.00-0.15 --proportions 0.85 0.00 0.15 &
+nohup python3 trainer.py --run-name 0.50-0.00-0.50 --proportions 0.50 0.00 0.50 &
+nohup python3 trainer.py --run-name 0.15-0.00-0.85 --proportions 0.15 0.00 0.85 &
+nohup python3 trainer.py --run-name 0.00-0.00-1.00 --proportions 0.00 0.00 1.00 &
+
