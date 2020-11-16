83286294aa0be00b796e7ce5d88f968a0278baa5
Tong Zhao <tongzhao997@gmail.com>
2020-11-15 19:54:23 -0600

    finished training initial experiment

--- constants.py
+++ constants.py
@@ -6,11 +6,10 @@ import os
 import pathlib
 
 # Directory containing 6883-vrp repo
-BASE_DIR = pathlib.Path(__file__).parent.absolute()
+# BASE_DIR = pathlib.Path(__file__).parent.absolute()
+# Does this solve the bug?
+BASE_DIR = pathlib.Path.cwd()
 
 # Directory for logs
 LOG_DIR = os.path.join(BASE_DIR, "log")
-
-
-
 
