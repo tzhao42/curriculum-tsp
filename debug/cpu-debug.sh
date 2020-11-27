415c8429747ac6f373ecd5cd0543d383277b6baa
Tong Zhao <tongzhao997@gmail.com>
2020-11-26 22:43:06 -0600

    ready for server

--- debug/cpu-debug.sh
+++ debug/cpu-debug.sh
@@ -3,4 +3,6 @@
 # Debugging script for debugging on my machine
 
 cd ../src
-python3 main.py --mode all --curriculum 4 --run-name debug-0 --debug --regen
+# python3 main.py --mode all --curriculum 4 --run-name debug-0 --debug --regen
+python3 main.py --mode test --curriculum 4 --debug --run-name debug --load tsp-20-1.00-0.00-0.00-20201114T193737
+
---
+++ jobs/exp4-6.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -t 48:00:00
+#SBATCH -N 1
+#SBATCH -n 4
+#SBATCH --mem=16G
+#SBATCH --gres=gpu:1
+#SBATCH --constraint=12GB
+#SBATCH -p normal
+
+# Setting up singularity variable
+SINGULARITY_CONTAINER="/om2/user/tzhao/singularity/ortools-pytorch-gpu.simg"
+STARTING_DIRECTORY="/om2/user/tzhao/6883/6883-vrp"
+
+cd ${STARTING_DIRECTORY}
+module load openmind/singularity
+hostname
+nvidia-smi
+
+singularity exec --nv ${SINGULARITY_CONTAINER} python3 src/main.py --mode all --run-name static-exp-4 --curriculum 4 &
+singularity exec --nv ${SINGULARITY_CONTAINER} python3 src/main.py --mode all --run-name static-exp-5 --curriculum 5 &
+singularity exec --nv ${SINGULARITY_CONTAINER} python3 src/main.py --mode all --run-name static-exp-6 --curriculum 6 &
+
+sleep 3h
+nvidia-smi
+sleep 48h
+
