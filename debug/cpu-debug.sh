e820735c0ebb2d4309bfe6989f2200c365ac03db
Tong Zhao <tongzhao997@gmail.com>
2020-11-27 00:18:31 -0600

    slight bug fix

--- debug/cpu-debug.sh
+++ debug/cpu-debug.sh
@@ -3,6 +3,6 @@
 # Debugging script for debugging on my machine
 
 cd ../src
-# python3 main.py --mode all --curriculum 4 --run-name debug-0 --debug --regen
-python3 main.py --mode test --curriculum 4 --debug --run-name debug --load tsp-20-1.00-0.00-0.00-20201114T193737
+python3 main.py --debug --mode all --curriculum 0 --run-name debug-0 --regen
+python3 main.py --debug --mode test --curriculum 4 --run-name debug-1 --load tsp-20-loader-00000000T000000
 
--- debug/singularity-debug.sh
+++ debug/singularity-debug.sh
@@ -3,6 +3,6 @@
 SINGULARITY_CONTAINER="/om2/user/tzhao/singularity/ortools-pytorch-gpu.simg"
 
 cd ..
-singularity exec --nv ${SINGULARITY_CONTAINER} python3 src/main.py --mode all --run-name debug-0 --curriculum 7 --debug
-# singularity exec --nv ${SINGULARITY_CONTAINER} python3 src/main.py --mode all --run-name debug-1 --curriculum 0 --regen 
+singularity exec --nv ${SINGULARITY_CONTAINER} python3 src/main.py --debug --mode all --run-name debug-0 --curriculum 7
+singularity exec --nv ${SINGULARITY_CONTAINER} python3 src/main.py --debug --mode all --run-name debug-1 --curriculum 0 --regen 
 
