b26da3fbde0c201ddc403d7644b5f448a677e490
Tong Zhao <tongzhao997@gmail.com>
2020-11-30 22:51:23 -0600

    oopser

--- jobs/run-all.sh
+++ jobs/run-all.sh
@@ -10,7 +10,7 @@ sbatch e20-regen-batch-01.sh
 sbatch e20-regen-batch-02.sh
 sbatch e20-regen-batch-03.sh
 sbatch e20-regen-batch-04.sh
-sbatch e100-static-exp-00.sh
+# sbatch e100-static-exp-00.sh
 # sbatch e100-static-exp-01.sh
 # sbatch e100-static-exp-02.sh
 # sbatch e100-static-exp-03.sh
